24552b7f054402d8c8c295e2d06418d185f406a7
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-25 21:41:06 +0100

    Update Portfolio.jsx

--- pages/components/Portfolio.jsx
+++ pages/components/Portfolio.jsx
@@ -2,18 +2,18 @@
 function Portfolio() {
  return(
     <div>
-        <h3 className="text-3xl py-1">
+        <h3 className="text-3xl py-1 dark:text-white">
             Portofolio
         </h3>
-        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800">
+        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800 dark:text-white">
             Since the begining of my journey as a frontnd web developer,
              I have created really beautiful website <span className="text-red-800 text-lg"><strong>userInterfaces</strong></span>,
               with rivaling <span className="text-red-800 text-lg"><strong>userExperiences</strong></span>. 
         </p>
-        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800">
+        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800 dark:text-white">
             I write brilliant articles on <a href="https://dumebi.hashnode.dev/" className="text-red-800 cusor-pointer">Hashnode</a> with the aim to teach and impact 
         </p>
-        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800">
+        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800 dark:text-white">
             I offer a wide rangle of services including <br />
              <a href="https://github.com/Dumebii" className="text-red-800 cusor-pointer text-lg font-black">frontend development,</a>
              <br /> <a href="https://dumebi.hashnode.dev/" className="text-red-800 cusor-pointer text-lg font-black">technical writing</a> 
