947b239fa6667475c301d1dd6f5e9929b712cbff
Dumebi Okolo <okolodumebi@gmail.com>
2022-10-25 20:41:22 +0100

    Update Services.jsx

--- pages/components/Services.jsx
+++ pages/components/Services.jsx
@@ -1,18 +1,18 @@
 function Services() {
     return(
        <div className="mt-8 ">
-        <h3 className='lg:text-4xl text-3xl py-1'>
+        <h3 className='lg:text-4xl text-3xl py-1 dark:text-white'>
             Services I offer
         </h3>
-        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800">
+        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800 dark:text-white">
             Since the begining of my journey as a frontnd web developer,
              I have created really beautiful website <span className="text-red-800 text-lg"><strong>userInterfaces</strong></span>,
               with rivaling <span className="text-red-800 text-lg"><strong>userExperiences</strong></span>. 
         </p>
-        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800">
+        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800 dark:text-white">
             I write brilliant articles on <a href="https://dumebi.hashnode.dev/" className="text-red-800 cusor-pointer">Hashnode</a> with the aim to teach and impact 
         </p>
-        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800">
+        <p className="lg:text-2xl text-md py-2 leading-8 text-grey-800 dark:text-white">
             I offer a wide rangle of services including <br />
              <a href="https://github.com/Dumebii" className="text-red-800 cusor-pointer text-lg font-black">frontend development,</a>
              <br /> <a href="https://dumebi.hashnode.dev/" className="text-red-800 cusor-pointer text-lg font-black">technical writing</a> 
